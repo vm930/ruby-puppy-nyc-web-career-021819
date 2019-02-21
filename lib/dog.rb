a690bfa113c3f4d3cabc15ed7f62415d59c55902
abstract-animation-9914 <vm930@hotmail.com>
2019-02-21 22:58:12 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog 
+  @@all = []
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all.each do |dog|
+      puts "#{dog.name}"
+    end 
+  end 
+  #need to fix this
+  
+  def self.clear_all
+    @@all.clear
+  end 
+  
+  def self.name
+    @name
+  end 
+end 
